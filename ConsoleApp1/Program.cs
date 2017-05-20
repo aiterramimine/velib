0bd4184f234a74385ddaf6d40d74811d58b6df65
Amine <aiterrami.amine10@gmail.com>
2017-05-20 11:21:16 +0800

    forget adding client

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using ConsoleApp1.VelibPlannerServiceReference;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VelibPlannerServiceClient client = new VelibPlannerServiceClient();
+            Console.WriteLine(client.ComputeRoute(null, null).duration);
+        }
+    }
+}
