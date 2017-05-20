7dedde1918119ce5d00bc3b75f69dd568e2a1f61
Amine <aiterrami.amine10@gmail.com>
2017-05-20 10:30:08 +0800

    VelibStation implem

--- velibPlanner/VelibStation.cs
+++ velibPlanner/VelibStation.cs
@@ -8,5 +8,39 @@ namespace velibPlanner
 {
     public class VelibStation
     {
+        /* The location of the station. */
+        private Location location;
+        /* Number of available vehicles in the station. */
+        private int availableVehicles;
+
+        public VelibStation(Location location, int availableVehicles)
+        {
+            this.location = location;
+            this.availableVehicles = availableVehicles;
+        }
+
+        /**
+         * Gets the location of the station.
+         */
+        public Location getLocation()
+        {
+            return location;
+        }
+
+        /**
+         * Gets the number of available vehicles in the station
+         */
+        public int getAvailableVehicle()
+        {
+            return availableVehicles;
+        }
+
+        /**
+         * True if there is at least on available vehicle in the station.
+         */
+        public bool isVehicleAvailable()
+        {
+            return availableVehicles > 0;
+        }
     }
 }
