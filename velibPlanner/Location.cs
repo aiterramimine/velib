3a2f4c7a88ce726562531e02947a5470fb70ae15
Amine <aiterrami.amine10@gmail.com>
2017-05-20 10:21:39 +0800

    first commit

---
+++ velibPlanner/Location.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace velibPlanner
+{
+    public class Location
+    {
+        /* Value of the latitude. */
+        private double latitude;
+        /* Value of the longitude. */
+        private double longitude;
+
+        /**
+         * Create a location from latitude and longitude.
+         */
+        public Location(double latitude, double longitude)
+        {
+            this.latitude = latitude;
+            this.longitude = longitude;
+        }
+
+        /**
+         * Gets the latitude
+         */
+        public double getLatitude()
+        {
+            return latitude;
+        }
+
+        /**
+         * Gets the longitude
+         */
+        public double getLongitude()
+        {
+            return longitude;
+        }
+    }
+}
---
+++ velibPlanner/VelibPlannerService.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace velibPlanner
+{
+    public class VelibPlannerService : IVelibPlannerService
+    {
+        private XmlDocument velibChart;
+
+        public Route ComputeRoute(Location current, Location destination)
+        {
+            return null;
+        }
+
+        /**
+         * Gets the nearest location of the parameter location.
+         */
+        private Location getNearestStation(Location l)
+        {
+            WebRequest request = WebRequest.Create("http://www.velib.paris/service/carto");
+
+            WebResponse response = request.GetResponse();
+
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+            string responseFromServer = reader.ReadToEnd();
+
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(responseFromServer);
+            XmlNodeList elemList = doc.GetElementsByTagName("marker");
+
+            for (int i = 0; i < elemList.Count; i++)
+            {
+                if (elemList[i].Attributes["fullAddress"].Value.Contains("75001"))
+                {
+                    String name = elemList[i].Attributes["name"].Value;
+                    Console.WriteLine(i + " : " + name);
+
+                }
+            }
+            Console.ReadLine();
+            reader.Close();
+            response.Close();
+            return null;
+        }
+
+        private List<VelibStation> getVelibStations()
+        {
+            refreshVelibChart();
+
+            List<VelibStation> ret = new List<VelibStation>();
+
+            return null;
+        }
+
+        private VelibStation getNearestVelibStationWithAvailableVehicle()
+        {
+            return null;
+        }
+
+        private VelibStation getNearestVelibStation()
+        {
+            return null;
+        }
+
+        /**
+         * Returns the velib chart in form of an XML document requested from the velib web service.
+         */
+        private void refreshVelibChart()
+        {
+            WebRequest request = WebRequest.Create("http://www.velib.paris/service/carto");
+
+            WebResponse response = request.GetResponse();
+
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+            string responseFromServer = reader.ReadToEnd();
+
+            XmlDocument velibChart = new XmlDocument();
+            velibChart.LoadXml(responseFromServer);
+
+            this.velibChart = velibChart;
+        }
+    }
+}
